b77f94b4ea95b1c31d0f7b21842bbd538c80140c
Sarwesh Saurabh <sarwesh.saurabh@in.ibm.com>
2016-02-12 19:28:04 +0100

    Added RestClient

--- src/main/java/com/ibm/timetracker/service/client/RestClient.java
+++ src/main/java/com/ibm/timetracker/service/client/RestClient.java
@@ -3,6 +3,7 @@ package com.ibm.timetracker.service.client;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -10,6 +11,13 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import com.ibm.timetracker.util.PropertiesFileUtil;
 
@@ -17,6 +25,7 @@ public class RestClient {
 	static final int connectionTimeout;
 	static final int socketTimeout;
 	static final String serverUrl;
+	static final RestTemplate restTemplate = new RestTemplate();
 	static
 	{
 		serverUrl = PropertiesFileUtil.getProperty("json.upload.server.url");
@@ -33,9 +42,14 @@ public class RestClient {
 			connTimeout = "3000";
 		}
 		connectionTimeout = Integer.parseInt(connTimeout);
+		SimpleClientHttpRequestFactory rf =
+			    (SimpleClientHttpRequestFactory) restTemplate.getRequestFactory();
+		rf.setReadTimeout(connectionTimeout);
+		rf.setConnectTimeout(socketTimeout);
+		
 	}
 
-	public static String postData(String jsonString) {
+	public static String postDataOldWay(String jsonString) {
 		HttpParams my_httpParams = new BasicHttpParams();
 		HttpConnectionParams.setConnectionTimeout(my_httpParams, connectionTimeout);
 		HttpConnectionParams.setSoTimeout(my_httpParams, socketTimeout);
@@ -63,6 +77,24 @@ public class RestClient {
 			return "fail";
 		}
 		return "success";
+	}
+	
+	public static String postData(String jsonString)
+	{
+		String plainCreds = "admin:admin";
+		byte[] plainCredsBytes = plainCreds.getBytes();
+		byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+		String base64Creds = new String(base64CredsBytes);
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Authorization", "Basic " + base64Creds);
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity<String> request = new HttpEntity<String>(jsonString, headers);
+			ResponseEntity<String> response = restTemplate.exchange(serverUrl, HttpMethod.POST, request, String.class);
+			return response.getBody();
+		} catch (Exception e) {
+			return "fail";
+		} 
 	}
 
 }
